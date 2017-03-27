638258ca46fba23e07de1ab955308e0ca6e1d2e7
Kacper Łukawski <ka.lukawski@gmail.com>
2017-03-27 21:35:21 +0200

    Finished stack mode

--- src/onePage.js
+++ src/onePage.js
@@ -2,6 +2,7 @@ class OnePage {
   constructor(options) {
     this.mode = options.mode
     this.animComplete = true
+    this.container = options.containerId
   }
 
   stackMode (elements) {
@@ -18,15 +19,11 @@ class OnePage {
     window.addEventListener('wheel', (e) => {
       if (!this.animComplete) return false
       
-      if (e.deltaY > 0) {
-        if (nextEl === elementsL - 1) return false
-        
+      if (e.deltaY > 0 && nextEl !== elementsL - 1) {        
         this.animComplete = false
         elements[nextEl].classList.toggle('outofview')
         nextEl++
-      } else {
-        if (nextEl === 0) return false
-        
+      } else if (e.deltaY < 0 && nextEl) {        
         this.animComplete = false
         nextEl--
         elements[nextEl].classList.toggle('outofview')
@@ -34,8 +31,10 @@ class OnePage {
     })
   }
 
-  normalMode (container) {
-    var translationValue = 0
+  normalMode (container, elements) {
+    const maxScrollCount = elements.length - 1
+    var translationValue = 0    
+    var scrollCount = 0
 
     container.addEventListener('transitionend', (e) => {
       this.animComplete = true
@@ -44,32 +43,33 @@ class OnePage {
     window.addEventListener('wheel', (e) => {
       if (!this.animComplete) return false
 
-      if (e.deltaY > 0) {        
+      if (e.deltaY > 0 && scrollCount !== maxScrollCount) {
         this.animComplete = false
         translationValue += -100
         container.style.transform = `translateY(${translationValue}vh)`
-        
-      } else {
+        scrollCount++
+      } else if (e.deltaY < 0 && scrollCount) {
         this.animComplete = false   
         translationValue += 100
         container.style.transform = `translateY(${translationValue}vh)`
+        scrollCount--
       }
     })
   }
 
   initPage () {
+    var elements = document.querySelectorAll('.section')
+
+    if (!elements.length) return console.error('Couldn\'t fetch elements')
+
     if (this.mode === 'stack') {
-      var elements = document.querySelectorAll('.section')
-
-      if (!elements.length) return console.error('Couldn\'t fetch elements')
-
       this.stackMode(elements)
     } else {
-      var container = document.querySelector('#container')
+      var container = document.querySelector(this.container)
       
       if (container === null) return console.error('Couldn\'t fetch element')
 
-      this.normalMode(container)
+      this.normalMode(container, elements)
     }
   }
 }
